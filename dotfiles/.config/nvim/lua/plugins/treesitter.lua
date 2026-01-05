bf4bbe56c374e017e7d2fbd3e39622e15939a54c
lzzrhx <casper.borretzen@tuta.com>
2026-01-05 14:53:03 +0100

    edit

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
+    main = 'nvim-treesitter',
     dependencies = {
       {
         'andymass/vim-matchup',
